a89ccdf8e547cd024e8fe59d04f2215c4eb277e2
Tiffany <tifa8579@hotmail.com>
2024-11-07 16:15:02 -0500

    Module 2 completed baseline

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
 //added travel Router path
 var travelRouter = require('./app_server/routes/travel');
 var handlebars = require('hbs');
@@ -13,7 +13,7 @@ var handlebars = require('hbs');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, "app_server", "views"));
 
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
